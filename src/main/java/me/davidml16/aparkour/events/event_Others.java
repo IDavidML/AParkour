68ec4ef0d571edd438f53aedc8eff13413100d92
DavidML <davidmolv16@gmail.com>
2020-03-17 01:29:28 +0100

    3.0.3

--- src/main/java/me/davidml16/aparkour/events/event_Others.java
+++ src/main/java/me/davidml16/aparkour/events/event_Others.java
@@ -9,12 +9,12 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 import me.davidml16.aparkour.Main;
@@ -22,112 +22,114 @@ import me.davidml16.aparkour.data.ParkourData;
 
 public class event_Others implements Listener {
 
-	@EventHandler
-	public void onBreak(BlockBreakEvent e) {
-		if (Main.getInstance().getTimerManager().hasPlayerTimer(e.getPlayer())) {
-			e.setCancelled(true);
-		}
-	}
+    @EventHandler
+    public void onBreak(BlockBreakEvent e) {
+        if (Main.getInstance().getTimerManager().hasPlayerTimer(e.getPlayer())) {
+            e.setCancelled(true);
+        }
+    }
 
-	@EventHandler
-	public void onPlace(BlockPlaceEvent e) {
-		if (Main.getInstance().getTimerManager().hasPlayerTimer(e.getPlayer())) {
-			e.setCancelled(true);
-		}
-	}
+    @EventHandler
+    public void onPlace(BlockPlaceEvent e) {
+        if (Main.getInstance().getTimerManager().hasPlayerTimer(e.getPlayer())) {
+            e.setCancelled(true);
+        }
+    }
 
-	@EventHandler
-	public void onHunger(FoodLevelChangeEvent e) {
-		if (Main.getInstance().getTimerManager().hasPlayerTimer((Player) e.getEntity())) {
-			e.setCancelled(true);
-		}
-	}
+    @EventHandler
+    public void onHunger(FoodLevelChangeEvent e) {
+        if (Main.getInstance().getTimerManager().hasPlayerTimer((Player) e.getEntity())) {
+            e.setCancelled(true);
+        }
+    }
 
-	@EventHandler
-	public void onDrop(final PlayerDropItemEvent e) {
-		if (Main.getInstance().getTimerManager().hasPlayerTimer(e.getPlayer())) {
-			e.setCancelled(true);
-			Bukkit.getScheduler().scheduleSyncDelayedTask(Main.getInstance(), new Runnable() {
-				public void run() {
-					e.getPlayer().updateInventory();
-				}
-			}, 1L);
-		}
-	}
+    @EventHandler
+    public void onDrop(final PlayerDropItemEvent e) {
+        if (Main.getInstance().getTimerManager().hasPlayerTimer(e.getPlayer())) {
+            e.setCancelled(true);
+            Bukkit.getScheduler().scheduleSyncDelayedTask(Main.getInstance(), new Runnable() {
+                public void run() {
+                    e.getPlayer().updateInventory();
+                }
+            }, 1L);
+        }
+    }
 
-	@EventHandler
-	public void inventoryClick(InventoryClickEvent e) {
-		if (Main.getInstance().getTimerManager().hasPlayerTimer((Player) e.getWhoClicked())) {
-			e.setCancelled(true);
-			((Player) e.getWhoClicked()).updateInventory();
-		}
-	}
+    @EventHandler
+    public void inventoryClick(InventoryClickEvent e) {
+        if (Main.getInstance().getTimerManager().hasPlayerTimer((Player) e.getWhoClicked())) {
+            e.setCancelled(true);
+            ((Player) e.getWhoClicked()).updateInventory();
+        }
+    }
 
-	@EventHandler
-	public void onDamage(EntityDamageEvent e) {
-		if(e.getEntity() instanceof Player) {
-			if (Main.getInstance().getTimerManager().hasPlayerTimer((Player) e.getEntity())) {
-				e.setCancelled(true);
-			}
-		}
-	}
+    @EventHandler
+    public void onDamage(EntityDamageEvent e) {
+        if (e.getEntity() instanceof Player) {
+            if (Main.getInstance().getTimerManager().hasPlayerTimer((Player) e.getEntity())) {
+                e.setCancelled(true);
+            }
+        }
+    }
 
-	@EventHandler
-	public void onPickup(PlayerPickupItemEvent e) {
-		if (Main.getInstance().getTimerManager().hasPlayerTimer(e.getPlayer())) {
-			e.setCancelled(true);
-		}
-	}
+    @EventHandler
+    public void onPickup(EntityPickupItemEvent e) {
+        if (e.getEntity() instanceof Player) {
+            if (Main.getInstance().getTimerManager().hasPlayerTimer((Player) e.getEntity())) {
+                e.setCancelled(true);
+            }
+        }
+    }
 
-	@EventHandler
-	public void onJoin(PlayerJoinEvent e) throws SQLException {
-		Player p = e.getPlayer();
-		Main.getInstance().getPlayerDataHandler().loadPlayerData(p);
-		Main.getInstance().getDatabaseHandler().updatePlayerName(p);
-		Main.getInstance().getStatsHologramManager().loadStatsHolograms(p);
-	}
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) throws SQLException {
+        Player p = e.getPlayer();
+        Main.getInstance().getPlayerDataHandler().loadPlayerData(p);
+        Main.getInstance().getDatabaseHandler().updatePlayerName(p);
+        Main.getInstance().getStatsHologramManager().loadStatsHolograms(p);
+    }
 
-	@EventHandler
-	public void onQuit(PlayerQuitEvent e) {
-		Player p = e.getPlayer();
+    @EventHandler
+    public void onQuit(PlayerQuitEvent e) {
+        Player p = e.getPlayer();
 
-		Main.getInstance().getStatsHologramManager().removeStatsHolograms(p);
+        Main.getInstance().getStatsHologramManager().removeStatsHolograms(p);
 
-		if (Main.getInstance().getStatsGUI().getOpened().contains(p.getUniqueId()))
-			Main.getInstance().getStatsGUI().getOpened().remove(p.getUniqueId());
-		
-		if (Main.getInstance().getStatsGUI().getGuis().containsKey(p.getUniqueId()))
-			Main.getInstance().getStatsGUI().getGuis().remove(p.getUniqueId());
+        if (Main.getInstance().getStatsGUI().getOpened().contains(p.getUniqueId()))
+            Main.getInstance().getStatsGUI().getOpened().remove(p.getUniqueId());
 
-		if (Main.getInstance().getTimerManager().hasPlayerTimer(e.getPlayer())) {
-			Main.getInstance().getTimerManager().cancelTimer(e.getPlayer());
+        if (Main.getInstance().getStatsGUI().getGuis().containsKey(p.getUniqueId()))
+            Main.getInstance().getStatsGUI().getGuis().remove(p.getUniqueId());
 
-			ParkourData parkour = Main.getInstance().getParkourHandler().getParkourByPlayer(p);
-			parkour.getPlayers().remove(p.getUniqueId());
+        if (Main.getInstance().getTimerManager().hasPlayerTimer(e.getPlayer())) {
+            Main.getInstance().getTimerManager().cancelTimer(e.getPlayer());
 
-			if (Main.getInstance().getConfig().getBoolean("RestartItem.Enabled")) {
-				Main.getInstance().getPlayerDataHandler().restorePlayerInventory(p);
-			}
-		}
+            ParkourData parkour = Main.getInstance().getParkourHandler().getParkourByPlayer(p);
+            parkour.getPlayers().remove(p.getUniqueId());
 
-		Main.getInstance().getPlayerDataHandler().getData(p).save();
-		Main.getInstance().getPlayerDataHandler().getPlayersData().remove(p.getUniqueId());
-	}
+            if (Main.getInstance().getConfig().getBoolean("RestartItem.Enabled")) {
+                Main.getInstance().getPlayerDataHandler().restorePlayerInventory(p);
+            }
+        }
 
-	@EventHandler
-	public void onWorld(PlayerChangedWorldEvent e) {
-		Player p = e.getPlayer();
-		Main.getInstance().getStatsHologramManager().reloadStatsHolograms(p);
+        Main.getInstance().getPlayerDataHandler().getData(p).save();
+        Main.getInstance().getPlayerDataHandler().getPlayersData().remove(p.getUniqueId());
+    }
 
-		if (Main.getInstance().getTimerManager().hasPlayerTimer(e.getPlayer())) {
-			Main.getInstance().getTimerManager().cancelTimer(e.getPlayer());
+    @EventHandler
+    public void onWorld(PlayerChangedWorldEvent e) {
+        Player p = e.getPlayer();
+        Main.getInstance().getStatsHologramManager().reloadStatsHolograms(p);
 
-			ParkourData parkour = Main.getInstance().getParkourHandler().getParkourByPlayer(p);
-			parkour.getPlayers().remove(p.getUniqueId());
+        if (Main.getInstance().getTimerManager().hasPlayerTimer(e.getPlayer())) {
+            Main.getInstance().getTimerManager().cancelTimer(e.getPlayer());
 
-			if (Main.getInstance().getConfig().getBoolean("RestartItem.Enabled")) {
-				Main.getInstance().getPlayerDataHandler().restorePlayerInventory(p);
-			}
-		}
-	}
+            ParkourData parkour = Main.getInstance().getParkourHandler().getParkourByPlayer(p);
+            parkour.getPlayers().remove(p.getUniqueId());
+
+            if (Main.getInstance().getConfig().getBoolean("RestartItem.Enabled")) {
+                Main.getInstance().getPlayerDataHandler().restorePlayerInventory(p);
+            }
+        }
+    }
 }
